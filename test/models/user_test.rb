7d70b925f4969e5f45a7f0b1cde1df6350e6232e
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:33:05 +0300

    User Model Test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: "Test User",
+      email: "user@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
