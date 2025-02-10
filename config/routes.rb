dbbed68bd9aca6ab63f3df66de2024042c28dbbd
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:18:25 +0300

    Routes Configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "books#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "signup", to: "users#new"
+  post "signup", to: "users#create"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :books do
+    post "borrow", on: :member
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :borrowings do
+    post "return", on: :member
+  end
+
+  get "profile", to: "users#show", as: "profile"
 end
