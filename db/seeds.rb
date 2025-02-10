94b9e90e31396f0b00a547a13c7b1d7e9813a579
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:39:51 +0300

    Created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Create test user
+User.create!(
+  name: "Test User",
+  email: "user@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+# Create 20 books
+20.times do
+  Book.create!(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    isbn: Faker::Code.isbn(base: 13)
+  )
+end
+
+# Create some borrowings
+user = User.first
+books = Book.limit(5)
+
+books.each do |book|
+  Borrowing.create!(
+    user: user,
+    book: book,
+    due_date: 2.weeks.from_now,
+    returned_at: rand(2).zero? ? Time.current : nil
+  )
+end
+
+puts "Created #{User.count} users"
+puts "Created #{Book.count} books"
+puts "Created #{Borrowing.count} borrowings"
