3697039c0a45751d3c5c122cf65f6104b261c11a
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:12:12 +0300

    Borrowing Model DB Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_10_160917) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_10_161123) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "isbn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "borrowings", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "due_date"
+    t.datetime "returned_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_borrowings_on_book_id"
+    t.index ["user_id"], name: "index_borrowings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +37,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_160917) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "borrowings", "books"
+  add_foreign_key "borrowings", "users"
 end
