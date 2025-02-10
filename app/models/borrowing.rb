a38e91af52eb7bb5273c085e38e38666e6ed67f9
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:13:14 +0300

    Borrowing Model Validations

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,4 +1,12 @@
 class Borrowing < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validate :book_availability, on: :create
+
+  private
+
+  def book_availability
+    errors.add(:book, "is not available") unless book.available?
+  end
 end
