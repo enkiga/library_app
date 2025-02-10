991ff30c105930dd0e932a8248a918e7f5a9a1f1
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:35:55 +0300

    Updated Book Controller Test

--- test/controllers/book_controller_test.rb
+++ test/controllers/book_controller_test.rb
@@ -1,8 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get books_path
-    assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @book = books(:one)
+  end
+
+  test "should borrow book" do
+    sign_in @user
+    assert_difference("Borrowing.count") do
+      post borrow_book_path(@book)
+    end
+    assert_redirected_to book_path(@book)
+  end
+
+  test "should prevent unauthorized borrowing" do
+    post borrow_book_path(@book)
+    assert_redirected_to login_path
   end
 end
