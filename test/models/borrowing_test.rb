cbdad18001e713923db04df315384b0d4a5e76f3
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:34:27 +0300

    Borrowing Model Test

--- test/models/borrowing_test.rb
+++ test/models/borrowing_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class BorrowingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @book = books(:one)
+  end
+
+  test "should create valid borrowing" do
+    borrowing = Borrowing.new(
+      user: @user,
+      book: @book,
+      due_date: 2.weeks.from_now
+    )
+    assert borrowing.valid?
+  end
+
+  test "should prevent duplicate active borrowings" do
+    Borrowing.create!(user: @user, book: @book, due_date: 2.weeks.from_now)
+    borrowing = Borrowing.new(user: @user, book: @book)
+    assert_not borrowing.valid?
+    assert_includes borrowing.errors[:book], "is not available"
+  end
+
+  test "should set default due date" do
+    borrowing = Borrowing.create!(user: @user, book: @book)
+    assert_in_delta 2.weeks.from_now, borrowing.due_date, 1.second
+  end
 end
