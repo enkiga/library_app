f7532b3c6f8c652c5f6dd8bc5a602b5ffab34fc8
Enoch Garoli <eenkiga@gmail.com>
2025-02-10 19:10:52 +0300

    Book Model Validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  validates :title, :author, :isbn, presence: true
+  validates :isbn, uniqueness: true
+  has_many :borrowings
+
+  def available?
+    borrowings.where(returned_at: nil).none?
+  end
 end
